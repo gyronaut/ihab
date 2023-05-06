d94fbc2cd1c4663316775903af7ca6420e00c707
Justin Blair <jbeta137@gmail.com>
2023-05-06 17:00:42 -0500

    made basic transaction entry function for ledger

--- ihab.py
+++ ihab.py
@@ -10,14 +10,10 @@ def create_connection(path):
         print(f"Error {e} has occured")
     return connection
 
-#FOREIGN KEY(account_from) REFERENCES account(account_id),
-#FOREIGN KEY(transaction_category) REFERENCES category(category_id))
-
 def init_databases(path):
     connection = create_connection(path)
     cursor = connection.cursor()
-
-   
+    #ledger table for transactions
     cursor.execute("CREATE TABLE IF NOT EXISTS ledger(" 
         "transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,"  
         "transaction_date TEXT," 
@@ -27,6 +23,7 @@ def init_databases(path):
         "transaction_comment TEXT,"
         "FOREIGN KEY(account_from) REFERENCES account(account_id),"
         "FOREIGN KEY(transaction_category) REFERENCES category(category_id))")
+    #account table for bank/credit accounts
     cursor.execute("CREATE TABLE IF NOT EXISTS account ("
         "account_id INTEGER PRIMARY KEY AUTOINCREMENT,"
         "account_name TEXT," 
@@ -34,6 +31,7 @@ def init_databases(path):
         "starting_account_balance REAL," 
         "current_account_balance REAL," 
         "account_comment TEXT)")
+    #budget category table
     cursor.execute("CREATE TABLE IF NOT EXISTS category("
         "category_id INTEGER PRIMARY KEY AUTOINCREMENT," 
         "category_name TEXT," 
@@ -42,6 +40,7 @@ def init_databases(path):
         "parent INTEGER," 
         "is_hidden BOOLEAN,"
         "FOREIGN KEY(parent) REFERENCES category(category_id))")
+    #table for transfers between categories
     cursor.execute("CREATE TABLE IF NOT EXISTS category_transfer("
         "transfer_id INTEGER PRIMARY KEY AUTOINCREMENT," 
         "transfer_date TEXT,"
@@ -50,6 +49,7 @@ def init_databases(path):
         "transfer_amount REAL,"
         "FOREIGN KEY(category_from) REFERENCES category(category_id),"
         "FOREIGN KEY(category_to) REFERENCES category(category_id))")
+    connection.close()
 
 def add_entry(cursor, table_name, data_string):
     return 0
@@ -57,7 +57,45 @@ def add_entry(cursor, table_name, data_string):
 def remove_entry(cursor, table_name, entry_id):
     return 0
 
-def edit_entry(cursor, tablie_name, entry_name, data_string):
+def edit_entry(cursor, table_name, entry_name, data_string):
     return 0
 
+def make_transaction(db, date, category, account, amount, comments):
+    data = (date, category, account, amount, comments)
+    connection = create_connection(db)
+    cursor = connection.cursor()
+    try:
+        cursor.execute('INSERT INTO ledger (transaction_date, transaction_category, account_from, transaction_amount, transaction_comment) VALUES (?, ?, ?, ?, ?)', data)
+    except Error as e:
+        print(e)
+    connection.commit();
+    connection.close()
+    return 0
+
+def make_category_transfer(db, date, category_from, category_to, amount):
+    return 0
+
+def create_account(db, name, account_type, starting_balance, comments):
+    return 0
+
+def create_category(db, name, parent_name, balance):
+    return 0
+
+def hide_category(name):
+    return 0
+
+
+
+def make_account_transfer(db, date, account_from, account_to, amount, comments):
+    make_transaction(db, date, "transfer", account_from, -1.0*amount, comments)
+    make_transaction(db, date, "transfer", account_to, amount, comments)
+
+
 init_databases("./test.db")
+make_transaction("./test.db", "2023-05-06", None, None, 10.0, "test transaction")
+
+connection = create_connection("./test.db");
+cursor = connection.cursor();
+cursor.execute("SELECT * FROM ledger");
+print(cursor.fetchall())
+
