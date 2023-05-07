9e8e4297820b7948f8cdc7da300220782ef81f3d
Justin Blair <jbeta137@gmail.com>
2023-05-06 19:58:01 -0500

    adding UNIQUE constraints, plus creation functions

--- ihab.py
+++ ihab.py
@@ -26,7 +26,7 @@ def init_databases(path):
     #account table for bank/credit accounts
     cursor.execute("CREATE TABLE IF NOT EXISTS account ("
         "account_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-        "account_name TEXT," 
+        "account_name TEXT UNIQUE," 
         "type TEXT," 
         "starting_account_balance REAL," 
         "current_account_balance REAL," 
@@ -34,7 +34,7 @@ def init_databases(path):
     #budget category table
     cursor.execute("CREATE TABLE IF NOT EXISTS category("
         "category_id INTEGER PRIMARY KEY AUTOINCREMENT," 
-        "category_name TEXT," 
+        "category_name TEXT UNIQUE," 
         "starting_category_balance REAL," 
         "current_category_balance REAL," 
         "parent INTEGER," 
@@ -51,9 +51,6 @@ def init_databases(path):
         "FOREIGN KEY(category_to) REFERENCES category(category_id))")
     connection.close()
 
-def add_entry(cursor, table_name, data_string):
-    return 0
-
 def remove_entry(cursor, table_name, entry_id):
     return 0
 
@@ -68,22 +65,51 @@ def make_transaction(db, date, category, account, amount, comments):
         cursor.execute('INSERT INTO ledger (transaction_date, transaction_category, account_from, transaction_amount, transaction_comment) VALUES (?, ?, ?, ?, ?)', data)
     except Error as e:
         print(e)
-    connection.commit();
+    connection.commit()
     connection.close()
-    return 0
 
 def make_category_transfer(db, date, category_from, category_to, amount):
-    return 0
+    data = (date, category_from, category_to, amount)
+    connection = create_connection(db)
+    cursor = connection.cursor()
+    try:
+        cursor.execute('INSERT INTO category_transfer (transfer_date, category_from, category_to, transfer_amount) VALUES (?, ?, ?, ?)', data)
+    except Error as e:
+        print(e)
+    connection.commit()
+    connection.close()
+
 
 def create_account(db, name, account_type, starting_balance, comments):
-    return 0
+    data = (name, account_type, starting_balance, starting_balance, comments)
+    connection = create_connection(db)
+    cursor = connection.cursor()
+    try:
+        cursor.execute('INSERT INTO category (category_name, type, starting_account_balance, current_account_balance, account_comment) VALUES (?, ?, ?, ?, ?)', data)
+    except Error as e:
+        print(e)
+    connection.commit()
+    connection.close()
 
-def create_category(db, name, parent_name, balance):
-    return 0
+def create_category(db, name, parent_category, balance):
+    data = (name, balance, balance, parent_category)
+    connection = create_connection(db)
+    cursor = connection.cursor()
+    try:
+        cursor.execute('INSERT INTO category (category_name, starting_category_balance, current_category_balance, parent, is_hidden) VALUES (?, ?, ?, ?, FALSE)', data)
+    except Error as e:
+        print(e)
+    connection.commit()
+    connection.close()
 
-def hide_category(name):
-    return 0
-
+def set_category_is_hidden(db, category_name, is_hidden):
+    connection = create_connection(db)
+    cursor = connection.cursor()
+    cursor.execute("UPDATE category "
+            "SET is_hidden = ? "
+            "WHERE category_name = ?", (is_hidden, category_name))
+    connection.commit()
+    connection.close()
 
 
 def make_account_transfer(db, date, account_from, account_to, amount, comments):
@@ -92,10 +118,11 @@ def make_account_transfer(db, date, account_from, account_to, amount, comments):
 
 
 init_databases("./test.db")
-make_transaction("./test.db", "2023-05-06", None, None, 10.0, "test transaction")
-
+#make_transaction("./test.db", "2023-05-06", None, None, 10.0, "test transaction")
+create_category("./test.db", "groceries", None, 100.00)
+set_category_is_hidden("./test.db", "groceries", True)
 connection = create_connection("./test.db");
 cursor = connection.cursor();
-cursor.execute("SELECT * FROM ledger");
+cursor.execute("SELECT * FROM category");
 print(cursor.fetchall())
 
