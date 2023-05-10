429e350d39f61b6c7681edaf8d0a25c5ccf1b29e
Justin Blair <jbeta137@gmail.com>
2023-05-09 23:26:00 -0500

    added functions to import budget and init category table. also updated budget and restructured to include separate parent_category table

--- ihab.py
+++ ihab.py
@@ -40,7 +40,12 @@ def init_databases(path):
         "current_category_balance REAL," 
         "parent INTEGER," 
         "is_hidden BOOLEAN,"
-        "FOREIGN KEY(parent) REFERENCES categories(category_id))")
+        "FOREIGN KEY(parent) REFERENCES parent_categories(parent_id))")
+    #budget parent category table
+    cursor.execute("CREATE TABLE IF NOT EXISTS parent_categories ("
+        "parent_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "parent_name TEXT UNIQUE,"
+        "is_hidden BOOLEAN)")
     #table for transfers between categories
     cursor.execute("CREATE TABLE IF NOT EXISTS category_transfers("
         "transfer_id INTEGER PRIMARY KEY AUTOINCREMENT," 
@@ -53,10 +58,8 @@ def init_databases(path):
     connection.commit()
     connection.close()
 
-def make_transaction(db, date, category, account, amount, comments):
+def make_transaction(cursor, date, category, account, amount, comments):
     data = (date, category, account, amount, comments)
-    connection = create_connection(db)
-    cursor = connection.cursor()
     try:
         cursor.execute('INSERT INTO ledger (transaction_date, transaction_category,'
             'account_from, transaction_amount, transaction_comment)'
@@ -64,82 +67,90 @@ def make_transaction(db, date, category, account, amount, comments):
             , data)
     except Error as e:
         print(e)
-    connection.commit()
-    connection.close()
 
-def make_category_transfer(db, date, category_from, category_to, amount):
+def make_category_transfer(cursor, date, category_from, category_to, amount):
     data = (date, category_from, category_to, amount)
-    connection = create_connection(db)
-    cursor = connection.cursor()
     try:
         cursor.execute('INSERT INTO category_transfers (transfer_date, category_from,'
             'category_to, transfer_amount)'
             'VALUES (?, ?, ?, ?)', data)
     except Error as e:
         print(e)
-    connection.commit()
-    connection.close()
 
-def create_account(db, name, account_type, starting_balance, current_balance, comments):
+def create_account(cursor, name, account_type, starting_balance, current_balance, comments):
     data = (name, account_type, starting_balance, current_balance, comments)
-    connection = create_connection(db)
-    cursor = connection.cursor()
     try:
         cursor.execute('INSERT INTO categories (category_name, type,'
             'starting_account_balance, current_account_balance, account_comment)'
             'VALUES (?, ?, ?, ?, ?)', data)
     except Error as e:
         print(f"Error {e}")
-    connection.commit()
-    connection.close()
 
-def create_category(db, name, parent_category, balance):
-    data = (name, balance, balance, parent_category)
-    connection = create_connection(db)
-    cursor = connection.cursor()
+def create_category(cursor, name, parent_category, starting_balance):
+    data = {"name": name, "balance": starting_balance, "parent": parent_category}
     try:
         cursor.execute('INSERT INTO categories (category_name, starting_category_balance,'
             'current_category_balance, parent, is_hidden)'
-            'VALUES (?, ?, ?, ?, FALSE)',data)
+            'VALUES (:name, :balance, :balance, :parent, FALSE)',data)
     except Error as e:
         print(e)
-    connection.commit()
-    connection.close()
 
-def set_category_is_hidden(db, category_name, is_hidden):
-    connection = create_connection(db)
-    cursor = connection.cursor()
+def create_parent(cursor, name):
+    try:
+        cursor.execute('INSERT INTO parent_categories (parent_name, is_hidden) '
+            'VALUES (?, ?)', (name, False))
+    except Error as e:
+        print(e)
+
+def set_category_is_hidden(cursor, category_name, is_hidden):
     cursor.execute("UPDATE categories "
         "SET is_hidden = ? "
-        "WHERE category_name = ? OR "
-        "( ? AND "
-        "category_id = (SELECT category_id FROM categories "
-        "WHERE parent = (SELECT category_id FROM categories "
-        "WHERE category_name = ?)))", 
-        (is_hidden, category_name, is_hidden, category_name))
-    connection.commit()
-    connection.close()
+        "WHERE category_name = ?", 
+        (is_hidden, category_name))
 
-def make_account_transfer(db, date, account_from, account_to, amount, comments):
-    make_transaction(db, date, "transfer", account_from, -1.0*amount, comments)
-    make_transaction(db, date, "transfer", account_to, amount, comments)
+def set_parent_is_hidden(cursor, parent_name, is_hidden):
+    data = {"parent": parent_name, "hidden": is_hidden}
+    cursor.execute("UPDATE parent_categories "
+            "SET is_hidden :hidden "
+            "WHERE parent_name = :parent "
+            "IF (:hidden) "
+            "BEGIN "
+                "UPDATE categories "
+                "SET is_hidden :hidden "
+                "WHERE parent_name = :parent"
+            "END",
+            data)
 
-def setup_budget_from_file(config_file):
+def make_account_transfer(cursor, date, account_from, account_to, amount, comments):
+    make_transaction(cursor, date, "transfer", account_from, -1.0*amount, comments)
+    make_transaction(cursor, date, "transfer", account_to, amount, comments)
+
+def read_budget_from_file(config_file):
     with open(config_file, 'r') as file:
         budget = yaml.safe_load(file)
     print(budget)
     for parent in budget['budget']:
         for child in parent['children']:
             print(parent['parent_category']+': '+child['category'])
+    return budget
 
+def make_categories_from_budget(cursor, budget):
+    for parent in budget['budget']:
+        create_parent(cursor, parent['parent_category'])
+        parent_id = cursor.execute("SELECT last_insert_rowid()").fetchone()[0]
+        for child in parent['children']:
+            create_category(cursor, child['category'], parent_id, child['budgeted'])
 
-init_databases("./test.db")
-#create_category("./test.db", "groceries", None, 100.00)
-#create_category("./test.db", "eggs", 1, 20.00)
-#set_category_is_hidden("./test.db", "groceries", True)
-#set_category_is_hidden("./test.db", "groceries", False)
-connection = create_connection("./test.db");
+path = "./test.db"
+init_databases(path)
+connection = create_connection(path);
 cursor = connection.cursor();
-cursor.execute("SELECT * FROM categories");
+budget = read_budget_from_file("budget.yaml")
+make_categories_from_budget(cursor, budget)
+connection.commit();
+cursor.execute("SELECT * FROM categories")
 print(cursor.fetchall())
-setup_budget_from_file("budget.yaml")
+cursor.execute("SELECT * FROM parent_categories")
+print(cursor.fetchall())
+connection.close()
+
